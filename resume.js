792d0dc2e0f059ad05b426aa63e232e03ac30eae
manas.pattanaik <manas.pattanaik@solarwinds.com>
2022-12-10 15:01:54 +0530

    add proj content generator

--- resume.js
+++ resume.js
@@ -1,6 +1,18 @@
-$(function(){
-    console.log("jQuery Loaded");
-    $.getJSON("./profile.json",function(data){
-        console.log(data);
-    });
+$(function () {
+  console.log("jQuery Loaded");
+  $.getJSON("./profile.json", function (data) {
+    var html = "";
+    for (var proj of data.projects) {
+      html += "<li><div><h3>" + proj.Name + "</h3>";
+      var tbl = "<table><tbody>";
+      for (var prop in proj) {
+        tbl += "<tr><th>" + prop + "</th><td>" + proj[prop] + "</td></tr>";
+      }
+      tbl += "</tbody></table>";
+      html += tbl;
+      html += "</div></li>";
+      console.log(proj);
+    }
+    $("#ulPros").html(html);
+  });
 });
