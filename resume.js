0e7c9942dae2481b26ddb226145c1f970d52f77d
manas.pattanaik <manas.pattanaik@solarwinds.com>
2022-12-11 14:57:07 +0530

    change

--- resume.js
+++ resume.js
@@ -1,11 +1,11 @@
 $(function () {
   console.log("jQuery Loaded");
   $.getJSON("./profile.json", function (data) {
-    fillProjects(data,"ulPros");
+    fillProjects(data, "ulPros");
     fillExps(data, "ulExps");
   });
 
-  function fillProjects(data, targetEleId){
+  function fillProjects(data, targetEleId) {
     var html = "";
     for (var proj of data.projects) {
       html += "<li><div><h3>" + proj.Name + "</h3>";
@@ -19,21 +19,19 @@ $(function () {
       html += tbl;
       html += "</div></li>";
     }
-    $("#"+targetEleId).html(html);
+    $("#" + targetEleId).html(html);
   }
 
-  function fillExps(data, targetEleId){
+  function fillExps(data, targetEleId) {
     var html = "";
-    for (var exp in data.exps) {
-      html+="<li>"+exp+"</li>";
+    for (let i = 0; i < data.exps.length; i++) {
+      html += "<li>" + exp + "</li>";
     }
-    $("#"+targetEleId).html(html);
+    $("#" + targetEleId).html(html);
   }
 
-  $("#linkDownload").click(function(){
-    const element = document.getElementById('resumeBody');
-        html2pdf()
-            .from(element)
-            .save();
+  $("#linkDownload").click(function () {
+    const element = document.getElementById("resumeBody");
+    html2pdf().from(element).save();
   });
 });
